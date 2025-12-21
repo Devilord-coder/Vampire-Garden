c54b1816a5e06494ffef9180acee87dd9c4d6647
ttan4ick <tyulina.tanechka@yandex.ru>
2025-12-21 20:53:47 +0300

    add main_map view to base window, restore prehistory

--- src/windows/prehistory_view.py
+++ src/windows/prehistory_view.py
@@ -8,19 +8,18 @@ class PrehistoryView(arcade.View):
     def __init__(self, window):
         super().__init__()
         self.window = window
-        # self.sound = reg.prehistory_voice
+        self.sound = reg.prehistory_voice
         self.ui_manager = arcade.gui.UIManager()
         self.ui_manager.enable()
 
     def setup(self):
         """ Метод настройки вида (чтение текста предыстрории из файла, загрузка картинок, включение озвучки,
         создание кнопки для продолжения) """
-
         with open("resources/prehistory.txt", "r") as file:
             self.text = file.readlines()
         self.background = arcade.load_texture("resources/Moon/png/orig.png")
         self.vampire_picture = arcade.load_texture("resources/prehistory_vampire.jpg")
-        # arcade.play_sound(self.sound, volume=0.5, loop=False)
+        arcade.play_sound(self.sound, volume=0.5, loop=False)
         
         part_x, part_y, center_x, center_y = self.window.get_parts()
 
@@ -38,16 +37,13 @@ class PrehistoryView(arcade.View):
 
     def continue_history(self, event):
         """ Метод переключения на следующий вид """
-
         print("OK")
-        ...
 
     def on_show_view(self):
         self.setup()
 
     def on_draw(self):
         """ Метод загрузки текста, картинки и кнопки """
-
         self.clear()
         rect = arcade.rect.XYWH(
             self.width // 2, self.height // 2, self.width, self.height
@@ -70,14 +66,13 @@ class PrehistoryView(arcade.View):
             text.draw()
             height -= 50
 
-        # rect = arcade.rect.XYWH(
-        #     self.width // 2 + 40 * self.width // 100, self.height // 2, 280, 500
-        # )
-        # arcade.draw_texture_rect(texture=self.vampire_picture, rect=rect)
+        rect = arcade.rect.XYWH(
+            self.width // 2 + 40 * self.width // 100, self.height // 2, 280, 500
+        )
+        arcade.draw_texture_rect(texture=self.vampire_picture, rect=rect)
 
         self.ui_manager.draw()
 
     def on_resize(self, width: float, height: float):
         """ Метод отработки изменения окна """
-
         super().on_resize(width, height)
